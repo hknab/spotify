cada98a4d9b5e14c82f47776e535f8ecf54ff21f
Abolfazl Abdali <abolfazlandali0@gmail.com>
2022-01-26 14:42:26 +0330

    context: initialize player actions

--- src/context/PlayerContext.js
+++ src/context/PlayerContext.js
@@ -3,7 +3,17 @@ import React from "react";
 const PlayerData = React.createContext();
 const PlayerSetData = React.createContext();
 
-const initialData = {};
+const initialData = {
+  playList: [], //playing playList
+  item: null, // item index
+  next: null, //next music item index
+  before: null, //before music item index
+  play: true,
+  time: 0,
+  shuffle: false,
+  repeat: false,
+  volume: 50,
+};
 
 function PlayerContext({ children }) {
   const [data, setData] = React.useState(initialData);
@@ -24,6 +34,16 @@ export function usePlayerSetState() {
 }
 export function usePlayerActions() {
   const setPlayerData = usePlayerSetState();
+  const setVolume = (vol) =>
+    setPlayerData((prev) => ({ ...prev, volume: vol }));
+  const setPlay = () =>
+    setPlayerData((prev) => ({ ...prev, play: !prev.play }));
+  const setRepeat = () =>
+    setPlayerData((prev) => ({ ...prev, repeat: !prev.repeat }));
+  const setShuffle = () =>
+    setPlayerData((prev) => ({ ...prev, shuffle: !prev.shuffle }));
+  const setTime = (time) => setPlayerData((prev) => ({ ...prev, time }));
+  return { setVolume, setPlay, setRepeat, setShuffle, setTime };
 }
 
 export default PlayerContext;
