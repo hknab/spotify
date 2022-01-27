35b7736a244ebae8b95035f2c0fc4532458236e5
Abolfazl Abdali <abolfazlandali0@gmail.com>
2022-01-27 19:55:51 +0330

    api: add and initiliaze axios client

---
+++ src/api/index.js
@@ -0,0 +1,87 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+/* -----------------------USER----------------------- */
+
+const getUser = async () => {
+  const { data } = await client.get("/user");
+  return data;
+};
+// const editUser = async (data) => {
+//     const { data } = await client.put('/user',data)
+//     return DataTransferItem
+// }
+const updateUser = async (user) => {
+  const { data } = await client.patch(`/user`, user);
+  return data;
+};
+/* -----------------------PLAY LIST----------------------- */
+const getPlayLists = async () => {
+  const { data } = await client.get("/playLists");
+  return data;
+};
+const getPlayList = async (id) => {
+  const { data } = await client.get(`/playLists/${id}`);
+  return data;
+};
+const createPlayList = async (playList) => {
+  const { data } = await client.post("/playLists", playList);
+  return data;
+};
+const updatePlayList = async (id, playList) => {
+  const { data } = await client.put(`/playLists/${id}`, playList);
+  return data;
+};
+const deletePlayList = async (id) => {
+  const { data } = await client.delete(`/playlists/${id}`);
+  return data;
+};
+
+/* -----------------------FOLDERS----------------------- */
+
+const getFolders = async () => {
+  const { data } = await client.get("/folders");
+  return data;
+};
+const getFolder = async (id) => {
+  const { data } = await client.get(`/folders/${id}`);
+  return data;
+};
+const createFolder = async (folder) => {
+  const { data } = await client.put("/folders", folder);
+  return data;
+};
+const updateFolder = async (id, folder) => {
+  const { data } = await client.put(`/folders/${id}`, folder);
+  return data;
+};
+const deleteFolder = async (id) => {
+  const { data } = await client.delete(`/folders/${id}`);
+  return data;
+};
+
+/* -----------------------MUSICS----------------------- */
+
+const getMusics = async () => {
+  const { data } = await client.get("/musics");
+  return data;
+};
+
+export {
+  getUser,
+  updateUser,
+  getPlayLists,
+  getPlayList,
+  createPlayList,
+  updatePlayList,
+  deletePlayList,
+  getFolders,
+  getFolder,
+  createFolder,
+  updateFolder,
+  deleteFolder,
+  getMusics,
+};
