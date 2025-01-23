8cd31e853a684637890715a929c907287895423f
HKNAB <abolfazlabdali0@gmail.com>
2025-01-23 05:58:43 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://spotify-backend-json-server.herokuapp.com/",
+  baseURL: "https://spotify-json-server-sand.vercel.app/",
 });
 
 /* -----------------------USER----------------------- */
